bd90456f481d019f2f4103323fa1657e0c9f0474
sam atman <atmanistan@gmail.com>
2013-05-17 21:39:37 -0700

    fiddling with the ruledef macro  On branch master

--- src/sandbox/compiler.clj
+++ src/sandbox/compiler.clj
@@ -1,21 +1,4 @@
 (ns sandbox.compiler)
-;we need to take something like this
-
-(def x "foo")
-
-(defn a-funky ;except defsomethingelse 
-	"a rule handler"
-	[x] ;x is not actually a variable, it is a symbol, in this case predefined to reference "foo"
-	(println  x))
-;and turn it into this
-
-(defn a-func
-  "a rule handler"
-	[rule cdr state] ;rule is :keyword, cdr is list or string, state is map,
-					   ;note that x is absent
-	(let [local-state state x x] ;we moved our literal x to the let statement 
-	  (println x) 
-	  local-state))
 
 ; so that we can repeatedly call a handler that uses the keyword to pull a-func
 ; (which would actually be gensymmed/anonymous) and call it, set! state to the return
@@ -27,18 +10,17 @@
 
 (def test-rule
      (def-rule-fn
-       (println "State! " (str state))
        (println "Key! " (str key))
+       (println "State! " (str state))
        (println "Root! " (str root))
        (println "Cdr! " (str cdr))))
                                    
 (defmacro def-rule-fn [& body]  
         `(fn [~'key ~'state ~'root ~'cdr]
            (let [~'local-state ~'state]
-           (println "Calling ...")
-          
+           (println "Calling ...")    
            ~@body
-           (inc ~'local-state))))    
+           (def ~'state (inc ~'local-state)))))
                       
            
 #_(defn defhandler [fn-name lets & body]
